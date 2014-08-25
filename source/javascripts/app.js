f6f9d23831ddc7783424cd4ca44cb3e5f047561d
slowbot <jesse@mistermachine.com>
2014-08-25 15:46:11 -0700

    updating dark overlay contrast class

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -5,17 +5,11 @@ $(document).ready(function() {
   
   var $menu = $('#header'),
   	  $menulink = $('#trigger-overlay');
-      $sharelink = $('#trigger-overlay-share');
 	
 	$menulink.click(function(e) {
 		e.preventDefault();
 		$menulink.toggleClass('active');
 	});
-
-  $sharelink.click(function(e) {
-    e.preventDefault();
-    $sharelink.toggleClass('active');
-  });
 
 	// Dock the header to the top of the window when scrolled past the banner.
     // This is the default behavior.
@@ -66,45 +60,6 @@ $(document).ready(function() {
 
 	triggerBttn.addEventListener( 'click', toggleOverlay );
 	closeBttn.addEventListener( 'click', toggleOverlay );
-
-var triggerBttn = document.getElementById( 'trigger-overlay-share' ),
-    overlay = document.querySelector( 'div.overlay-share' ),
-    closeBttn = overlay.querySelector( 'a.overlay-close' );
-    transEndEventNames = {
-      'WebkitTransition': 'webkitTransitionEnd',
-      'MozTransition': 'transitionend',
-      'OTransition': 'oTransitionEnd',
-      'msTransition': 'MSTransitionEnd',
-      'transition': 'transitionend'
-    },
-    transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
-    support = { transitions : Modernizr.csstransitions };
-
-  function toggleOverlay() {
-    if( classie.has( overlay, 'open' ) ) {
-      classie.remove( overlay, 'open' );
-      classie.add( overlay, 'close' );
-      var onEndTransitionFn = function( ev ) {
-        if( support.transitions ) {
-          if( ev.propertyName !== 'visibility' ) return;
-          this.removeEventListener( transEndEventName, onEndTransitionFn );
-        }
-        classie.remove( overlay, 'close' );
-      };
-      if( support.transitions ) {
-        overlay.addEventListener( transEndEventName, onEndTransitionFn );
-      }
-      else {
-        onEndTransitionFn();
-      }
-    }
-    else if( !classie.has( overlay, 'close' ) ) {
-      classie.add( overlay, 'open' );
-    }
-  }
-
-  triggerBttn.addEventListener( 'click', toggleOverlay );
-  closeBttn.addEventListener( 'click', toggleOverlay );
 });
 
 $(function(){
