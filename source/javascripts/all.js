c71e85bcf55fe1281a347c7d9ecf9aa8017dec2b
slowbot <jesse@slowbot.com>
2014-10-15 18:30:28 -0700

    Updates to pattern library homepage

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,7 +1,5 @@
 //= require jquery/dist/jquery
 //= require foundation/js/foundation.min
-////= require video.js/dist/video-js/video.js
-////= require bigvideo.js/lib/bigvideo.js
 
 //= require jquery-scrolltofixed
 //= require classie
